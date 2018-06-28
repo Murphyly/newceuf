021c11eef6ce63b353aac86c7327219a95f16f9b
Murphyly <isysnogueira@infojr.com.br>
2018-06-28 13:39:45 -0300

    adiciona layouts temporarios

---
+++ resources/views/events/new.blade.php
@@ -0,0 +1,8 @@
+@extends ("layouts.layout-main")
+
+@section('title')
+    Criar evento
+@endsection
+
+@section('content')
+@endsection
---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script language="JavaScript" type="text/javascript" src="{{ asset('js/bootstrap.js')}}"></script>
+<script language="JavaScript" type="text/javascript" src="{{ asset('vendor/jQuery-Mask-Plugin-master/dist/jquery.mask.js') }}"></script>
+<script language="JavaScript" type="text/javascript" src="{{ asset('vendor/laravel-filemanager/js/lfm.js') }}"></script>
+<script language="JavaScript" type="text/javascript" src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script language="JavaScript" type="text/javascript" src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
