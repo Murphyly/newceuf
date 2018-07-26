31230341cfc2b000c008272fcd92c8620bfa6b6a
Murphyly <isysnogueira@infojr.com.br>
2018-07-26 11:30:55 -0300

    altera update eventcontroller

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -83,7 +83,7 @@ class EventController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Event $event)
+    public function update(Request $request, Events $event)
     {
         $event->update($request->all());
  
